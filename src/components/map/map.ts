31d4557f633e5d10f8a07276f0c97a3aeb35e258
Arm Suwansiri <ysuwansiri@yahoo.com>
2016-12-05 23:03:34 +0700

    added navigation button

--- src/components/map/map.ts
+++ src/components/map/map.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Injectable } from '@angular/core';
+import { LoadingController } from 'ionic-angular';
+import { Geolocation } from 'ionic-native';
+import {Observable} from 'rxjs/Rx';
 
 /*
   Generated class for the Map component.
@@ -10,6 +13,8 @@ import { Component, OnInit, Input } from '@angular/core';
   selector: 'map',
   templateUrl: 'map.html'
 })
+
+@Injectable()
 export class MapComponent implements OnInit {
 
   text: string;
@@ -17,13 +22,68 @@ export class MapComponent implements OnInit {
 
   @Input() isPickupRequested: boolean;
 
-  constructor() {
+  constructor(private loadingCtrl: LoadingController) {
     console.log('Hello Map Component');
     this.text = 'Hello World';
+    
   }
 
   ngOnInit() {
     this.map = this.createMap();
+    this.getCurrentLocation().subscribe(
+      location => {
+        this.centerLocation(location);
+      }
+    )
+  }
+
+  centerLocation(location) {
+    if (location) {
+      this.map.panTo(location);
+    } else {
+      this.getCurrentLocation().subscribe(
+        currentLocation => {
+          this.map.panTo(currentLocation);
+        }
+      );
+    }
+  }
+
+  getCurrentLocation(): Observable<any> {
+
+    let loading = this.loadingCtrl.create({
+      content: 'Locating...'
+    });
+
+    loading.present();
+    
+    let options = {
+      timeout: 10000,
+      enableHighAccuracy: true
+    };
+
+    let locationObs = Observable.create(observable => {
+      Geolocation.getCurrentPosition(options)
+      .then(
+        resp => {
+          let lat = resp.coords.latitude;
+          let lng = resp.coords.longitude;
+
+          let location = new google.maps.LatLng(lat, lng);
+          observable.next(location);
+          loading.dismiss();
+        },
+        err => {
+          console.log(err); 
+          loading.dismiss();
+        }
+      );
+    }
+    );
+
+    return locationObs;
+
+    
   }
 
   createMap(location = new google.maps.LatLng(40.712784, -74.005941)) {
